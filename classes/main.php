aec57a28152056763070d720588e6f079a30e03a
cp6 <corbin@corbpie.com>
2020-07-26 13:32:48 +1000

    Added MySQL connect function
    Added MySQL connect function: db_connect()

--- classes/main.php
+++ classes/main.php
@@ -2,5 +2,19 @@
 
 class main
 {
-
+    public function db_connect(bool $select_only = false, string $db_host = '127.0.0.1'): object
+    {
+        if ($select_only) {
+            $db_user = 'select_only';
+            $db_password = '';
+        } else {
+            $db_user = 'root';
+            $db_password = '';
+        }
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+        return new PDO("mysql:host=$db_host;dbname=contenie;charset=utf8mb4", $db_user, $db_password, $options);
+    }
 }
