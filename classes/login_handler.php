a11125ae90597b10788b8279a2e492b7b655cd2a
cp6 <corbin@corbpie.com>
2020-07-27 11:36:04 +1000

    Added login_handler.php

---
+++ classes/login_handler.php
@@ -0,0 +1,110 @@
+<?php
+require_once('main.php');
+
+class loginHandler extends main
+{
+    private string $username;
+    private string $stated_password;
+    private string $real_password;
+    private string $ip_address;
+    public int $uid;
+
+    public function __construct(string $username, string $password)
+    {
+        $this->username = $username;
+        $this->stated_password = $password;
+        $this->ip_address = $_SERVER['REMOTE_ADDR'];
+    }
+
+    public function getUserData(): bool
+    {
+        $db = $this->db_connect();
+        $select = $db->prepare("SELECT `uid`, `username`, `password` FROM `accounts` WHERE `username` = ? LIMIT 1;");
+        $select->execute([$this->username]);
+        if ($select->rowCount() > 0) {//Row found for username
+            $result = $select->fetch();
+            $this->uid = $result['uid'];
+            $this->real_password = $result['password'];
+            return true;
+        } else {//Username not found
+            return false;
+        }
+    }
+
+    public function checkPasswordCorrect(): bool
+    {
+        if (password_verify($this->stated_password, $this->real_password)) {
+            return true;//Password is correct
+        } else {
+            return false;//Bad password
+        }
+    }
+
+    public function doLoginSuccess(): void
+    {
+        $db = $this->db_connect();
+        $update = $db->prepare("UPDATE `accounts` SET `login_count` = (login_count + 1), `last_login_at` = NOW(), `last_login_ip` = ? WHERE `uid` = ? LIMIT 1;");
+        $update->execute([$this->ip_address, $this->uid]);
+    }
+
+    public function addLoginFailCount(): void
+    {
+        $db = $this->db_connect();
+        $update = $db->prepare("UPDATE `accounts` SET login_fails = (login_fails + 1), `last_fail` = NOW() WHERE `username` = ? LIMIT 1;");
+        $update->execute([$this->username]);
+    }
+
+    public function addLoginFailAttempt(): void
+    {
+        $db = $this->db_connect();
+        $insert = $db->prepare('INSERT IGNORE INTO `login_attempts` (`username`, `ip`) VALUES (?, ?)');
+        $insert->execute([$this->username, $this->ip_address]);
+    }
+
+    public function getRecentFailCount(): int
+    {
+        $db = $this->db_connect();
+        $select = $db->prepare("SELECT COUNT(*) as the_count FROM `login_attempts` WHERE `ip` = ? AND `datetime` > (NOW() - INTERVAL 10 MINUTE);");
+        $select->execute([$this->ip_address]);
+        return $select->fetch()['the_count'];//login fails for IP in the last 10 minutes
+    }
+
+    public function attemptLogin(string $redirect_to = '')
+    {
+        if ($this->getRecentFailCount() >= main::FAIL_ATTEMPTS_ALLOWED) {//IP has had X or more fails in last 10 minutes
+            return "IP Address has been locked for 10 minutes";
+        }
+        if ($this->getUserData()) {//Username found
+            if ($this->checkPasswordCorrect()) {//Password is correct
+                $this->doLoginSuccess();
+                $this->startSession();
+                $this->setSession('user', $this->uid);//Set session as uid
+                $this->redirectTo($redirect_to);
+            } else {//Password is wrong
+                $this->addLoginFailCount();//Add 1 onto login fail count
+                $this->addLoginFailAttempt();//ip and datetime into login attempt fail logs
+                //return "Password is wrong for {$this->username}";//Dont use this, helps brute forcing.
+                return "Failed login";//Be vague in error response
+            }
+        } else {
+            //return "Username: {$this->username} not found in DB";
+            return "Failed login";//Be vague in error response
+        }
+    }
+
+    public function loginForm()
+    {
+        $this->pageHead('Login', 'Login to website name', '../', true, false);
+        $this->tagOpen('div', 'container');
+        $this->formBuilder('POST', 'check_login.php');
+        $this->inputBuilder('text', 'login', '', 'username');
+        $this->tagOpen('div', 'form-group');
+        $this->inputBuilder('password', 'password');
+        $this->tagClose('div');
+        $this->formButtonBuilder('Login');
+        $this->tagClose('form');
+        $this->outputString("</div></body></html>");
+    }
+
+
+}
