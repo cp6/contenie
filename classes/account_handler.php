03894e8b7573d6c1862d2c13a13d61e51009f416
cp6 <corbin@corbpie.com>
2020-07-27 15:22:25 +1000

    Added account types

--- classes/account_handler.php
+++ classes/account_handler.php
@@ -6,6 +6,10 @@ class accountHandler extends main
     public string $username;
     public int $uid;
     public array $user_data;
+    public bool $is_admin = false;
+    public bool $is_mod = false;
+    public bool $is_subbed_user = false;
+    public bool $is_user = false;
 
     public function setUserData()
     {
@@ -16,6 +20,7 @@ class accountHandler extends main
             $select->execute([$this->uid]);
             $this->user_data = $select->fetchAll(PDO::FETCH_ASSOC)[0];
             $this->setUsername();
+            $this->setUserType();
         } else {
             $this->redirectTo('../login/');
         }
@@ -24,6 +29,20 @@ class accountHandler extends main
     public function setUsername()
     {
         $this->username = $this->user_data['username'];
+    }
+
+    public function setUserType()
+    {
+        $ac_type = $this->user_data['account_type'];
+        if ($ac_type == 1) {
+            $this->is_user = true;
+        } elseif ($ac_type == 2) {
+            $this->is_subbed_user = true;
+        } elseif ($ac_type == 3) {
+            $this->is_mod = true;
+        } elseif ($ac_type == 4) {
+            $this->is_admin = true;
+        }
     }
 
     public function accountHomepage()
