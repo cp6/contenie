29ec5f96c7f7984001e3dcc15fee54870280515c
cp6 <cp6@users.noreply.github.com>
2024-03-17 21:34:59 +1100

    Updated Media columns

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Media extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['sid', 'user_id', 'type', 'visibility', 'has_versions', 'size_kb', 'duration_seconds', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio'];
+    protected $fillable = ['sid', 'user_id', 'type', 'visibility', 'ext', 'has_versions', 'size_kb', 'duration_seconds', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio', 'original_name', 'title'];
+
+    protected static function booted(): void
+    {
+        static::creating(function (Media $media) {
+            $media->sid = Str::random(8);
+        });
+
+    }
 
 }
--- database/migrations/2024_03_17_102609_create_media_table.php
+++ database/migrations/2024_03_17_102609_create_media_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('media', function (Blueprint $table) {
@@ -14,6 +13,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->tinyInteger('type');
             $table->tinyInteger('visibility')->default(0);//0 = hidden, 1 = public, 3 = restricted, 4 = paid
+            $table->string('ext', 4);
             $table->boolean('has_versions');
             $table->integer('size_kb');
             $table->integer('duration_seconds')->nullable()->default(null);
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->integer('height')->nullable()->default(null);
             $table->integer('width')->nullable()->default(null);
             $table->boolean('has_audio')->nullable()->default(null);
+            $table->string('original_name');
+            $table->string('title', 32);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
