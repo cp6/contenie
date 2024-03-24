f41f66c71a1a623eb8fec5e725bd9d6c208732d7
cp6 <cp6@users.noreply.github.com>
2024-03-24 21:02:49 +1100

    Added upload and meta relations to Media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -10,9 +10,9 @@ class Media extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['sid', 'user_id', 'upload_id', 'parent_id', 'type', 'visibility', 'ext', 'audio_streams', 'directory_id', 'size_kb', 'duration', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio', 'aspect_ratio', 'title', 'mime', 'codec'];
+    protected $fillable = ['sid', 'user_id', 'upload_id', 'parent_id', 'type', 'visibility', 'ext', 'audio_streams', 'directory_id', 'size_kb', 'duration', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio', 'aspect_ratio', 'mime', 'codec'];
 
-    protected $with = ['directory', 'audio'];
+    protected $with = ['directory', 'upload', 'meta', 'audio'];
 
     protected static function booted(): void
     {
@@ -21,9 +21,19 @@ class Media extends Model
         });
     }
 
+    public function upload(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Upload::class, 'id', 'upload_id');
+    }
+
     public function directory(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
         return $this->hasOne(Directory::class, 'id', 'directory_id');
+    }
+
+    public function meta(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Meta::class, 'media_id', 'id');
     }
 
     public function audio(): \Illuminate\Database\Eloquent\Relations\HasMany
