c08b2b91c65cb16aea721867d81b1005e9bc2981
cp6 <cp6@users.noreply.github.com>
2024-03-31 23:51:23 +1100

    Cleaned up ProcessController.php

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -6,8 +6,10 @@ use App\Models\Audio;
 use App\Models\Media;
 use App\Models\Process;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProcessController extends Controller
 {
@@ -36,6 +38,7 @@ class ProcessController extends Controller
         $media = Media::where('sid', $process->media_sid)->firstOrFail();
         $thumbnail = Process::createThumbnail($media);
         $format_details = Process::output($media);
+
         if (isset($format_details['video'])) {
             $media->update([
                 'audio_streams' => $format_details['audio_streams'],
@@ -46,39 +49,11 @@ class ProcessController extends Controller
                 'width' => $format_details['video']['width'],
                 'aspect_ratio' => $format_details['video']['aspect_ratio'],
                 'duration' => $format_details['video']['duration'],
-                'has_audio' => ($format_details['audio_streams'] > 0) ? 1 : 0
+                'has_audio' => ($format_details['audio_streams'] > 0) ? 1 : 0,
             ]);
         }
 
-        if (isset($format_details['audio'])) {
-
-            $index = 0;
-            foreach ($format_details['audio'] as $stream) {
-
-                try {
-                    Audio::create([
-                        'media_id' => $media->id,
-                        'index' => $index,
-                        'codec' => $stream['codec'],
-                        'profile' => $stream['profile'],
-                        'channels' => $stream['channels'],
-                        'rate' => $stream['rate'],
-                        'layout' => $stream['layout'],
-                        'size_kb' => $stream['size_kb'],
-                        'duration' => $stream['duration'],
-                        'bitrate_kbs' => $stream['bitrate_kbps'],
-                        'timebase' => $stream['timebase'],
-                        'name' => $stream['handler_name'] ?? null,
-                    ]);
-                } catch (\Exception $exception) {
-                    Log::debug($exception->getMessage());
-                }
-                $index++;
-            }
-
-        }
-
-        return view('process.edit', ['media' => $media]);
+        return view('process.edit', ['media' => $media, 'ratios' => Process::ratios169()]);
         //$file = Storage::disk('public')->path("temp/{$process->media_sid}.mp4");
         //$command = 'ffprobe -v error -show_format -show_streams -print_format json ' . $file;
         //return json_decode(shell_exec($command), true);
@@ -93,4 +68,40 @@ class ProcessController extends Controller
     {
         //
     }
+
+    public function thumbnailForTrim(Request $request, Process $process)
+    {
+        $type = $request->type;
+        Log::debug($request->time_string);
+        $seconds = array_reduce(explode(':', $request->time_string), function ($carry, $item) {
+            return $carry * 60 + $item;
+        });
+        Log::debug($seconds);
+        $file = Storage::disk('public')->path("temp/{$request->sid}.{$request->ext}");
+        $save_as = storage_path("app/public/temp/{$request->sid}_{$type}.jpg");
+
+        $command = "ffmpeg -y -i $file -ss $seconds -vframes 1 $save_as";
+        Log::debug($command);
+        return shell_exec($command);
+    }
+
+    public function doTrim(Request $request, Process $process)
+    {
+        //dd($process);
+        $start = $request->start_value;
+        $finish = $request->finish_value;
+        $file = Storage::disk('public')->path("temp/{$process->media_sid}.{$process->media->ext}");
+        $save_as = storage_path("app/public/temp/{$process->media_sid}_RENAME.{$process->media->ext}");
+
+        $command = "ffmpeg -y -ss $start -i $file -t $finish -c copy $save_as";
+        Log::debug($command);
+        Log::debug("temp/{$process->media_sid}_RENAME.{$process->media->ext}");
+        Log::debug("temp/{$process->media_sid}.{$process->media->ext}");
+
+        $trim = shell_exec($command);
+
+        Storage::disk('public')->move("temp/{$process->media_sid}_RENAME.{$process->media->ext}", "temp/{$process->media_sid}.{$process->media->ext}");
+        return $trim;
+    }
+
 }
