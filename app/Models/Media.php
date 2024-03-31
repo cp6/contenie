8b400150bf543bce128e306ab258cdde95c8a043
cp6 <cp6@users.noreply.github.com>
2024-03-31 22:52:01 +1100

    Removed audio relation from Media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -10,9 +10,9 @@ class Media extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['sid', 'user_id', 'upload_id', 'parent_id', 'type', 'visibility', 'ext', 'audio_streams', 'directory_id', 'size_kb', 'duration', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio', 'aspect_ratio', 'mime', 'codec'];
+    protected $fillable = ['sid', 'user_id', 'upload_id', 'parent_id', 'type', 'visibility', 'ext', 'audio_streams', 'directory_id', 'size_kb', 'duration', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio', 'aspect_ratio', 'mime', 'codec', 'rate', 'channels', 'layout', 'timebase'];
 
-    protected $with = ['directory', 'upload', 'meta', 'audio'];
+    protected $with = ['directory', 'upload', 'meta'];
 
     protected static function booted(): void
     {
@@ -34,11 +34,6 @@ class Media extends Model
     public function meta(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
         return $this->hasOne(Meta::class, 'media_id', 'id');
-    }
-
-    public function audio(): \Illuminate\Database\Eloquent\Relations\HasMany
-    {
-        return $this->hasMany(Audio::class, 'media_id', 'id');
     }
 
 
