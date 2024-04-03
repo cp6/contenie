c16c0e5900a942c415a42051739e6d1072b80e33
cp6 <cp6@users.noreply.github.com>
2024-04-03 22:43:16 +1100

    Removed upload relation from with in Media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -12,7 +12,7 @@ class Media extends Model
 
     protected $fillable = ['sid', 'user_id', 'upload_id', 'parent_id', 'type', 'visibility', 'ext', 'audio_streams', 'directory_id', 'size_kb', 'duration', 'bitrate_kbs', 'framerate', 'height', 'width', 'has_audio', 'aspect_ratio', 'mime', 'codec', 'rate', 'channels', 'layout', 'timebase'];
 
-    protected $with = ['directory', 'upload', 'meta'];
+    protected $with = ['directory', 'meta'];
 
     protected static function booted(): void
     {
