958ad34c5ef70d3b66e802e1e81cb09a147d21a2
cp6 <cp6@users.noreply.github.com>
2024-04-03 22:42:44 +1100

    Updated UploadController.php

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -40,9 +40,10 @@ class UploadController extends Controller
             $store_temp = $file->storeAs('public/temp', $sid . '.' . $ext);
 
             if ($store_temp) {
-                //Storing file was a success
+                //Storing file in temp was a success
                 try {
                     $upload = new Upload();
+                    $upload->sid = $sid;
                     $upload->user_id = Auth::id();
                     $upload->original_name = $file->getClientOriginalName();
                     $upload->save();
@@ -89,25 +90,66 @@ class UploadController extends Controller
                     if (Storage::disk('public')->exists('temp/' . $sid . '.' . $ext)) {
                         Storage::disk('public')->delete('temp/' . $sid . '.' . $ext);
                     }
-                    return response()->json(['id' => null]);
+
+                    return response()->json(['sid' => null]);
                 }
 
-
-                return response()->json(['id' => $process->id]);
+                return response()->json(['sid' => $sid]);
 
             }
 
-            return response()->json(['id' => null]);
-            //return redirect()->route('dashboard')->with('success', 'Created media');
+            return response()->json(['sid' => null]);
         }
         //Doesnt have file in request
-        return response()->json(['id' => null]);
+        return response()->json(['sid' => null]);
 
     }
 
     public function show(Upload $upload)
     {
         //
+    }
+
+    public function editStepOne(Upload $upload)
+    {
+        if ($upload->media->type === 1) {//Video
+            $thumbnail = Process::createThumbnail($upload->media);
+            $format_details = Process::output($upload->media);
+
+            if (isset($format_details['video'])) {
+                $upload->media->update([
+                    'audio_streams' => $format_details['audio_streams'],
+                    'bitrate_kbs' => $format_details['video']['bitrate_kbs'],
+                    'framerate' => (int)$format_details['video']['framerate'],
+                    'codec' => $format_details['video']['codec'],
+                    'height' => $format_details['video']['height'],
+                    'width' => $format_details['video']['width'],
+                    'aspect_ratio' => $format_details['video']['aspect_ratio'],
+                    'duration' => $format_details['video']['duration'],
+                    'has_audio' => ($format_details['audio_streams'] > 0) ? 1 : 0,
+                ]);
+            }
+
+            return view('process.trim', ['media' => $upload->media]);
+        }
+
+        return redirect()->route('upload.versions');
+
+    }
+
+    public function editStepTwo(Upload $upload)
+    {
+        if ($upload->media->type === 1) {//Video
+            return view('process.compress', ['media' => $upload->media, 'ratios' => Process::ratios169()]);
+        }
+
+        return redirect()->route('upload.meta');
+
+    }
+
+    public function editStepThree(Upload $upload)
+    {
+        return view('process.meta', ['media' => $upload->media]);
     }
 
     public function edit(Upload $upload)
