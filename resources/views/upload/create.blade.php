b0acfc9cb2f8ce73f5846b37aebef869ad1c9054
cp6 <cp6@users.noreply.github.com>
2024-04-07 21:55:15 +1000

    Added an attractive upload input

--- resources/views/upload/create.blade.php
+++ resources/views/upload/create.blade.php
@@ -6,24 +6,43 @@
     </x-slot>
 
     <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="px-8 pt-6 pb-8 mb-6">
-                    <form id="uploadForm" action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                    <div class="mb-6">
-                        <label class="block text-gray-700 dark:text-gray-100 text-sm font-bold mb-2" for="file">
-                            Choose your file
+                    <div class="flex items-center justify-center w-full">
+                        <label for="fileInput"
+                               class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                </svg>
+                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span>
+                                    or drag and drop</p>
+                                <p class="text-xs text-gray-500 dark:text-gray-400">Video, image, or audio file (Max 6GB)</p>
+                            </div>
+
+                            <form id="uploadForm" action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="mb-6">
+                                    <input id="fileInput" name="file" type="file" class="hidden" required/>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <button
+                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-8 rounded focus:outline-none focus:shadow-outline"
+                                        type="submit">
+                                        Upload
+                                    </button>
+                                </div>
+                            </form>
+
                         </label>
-                        <input class="appearance-none rounded w-full py-2 px-1 text-gray-700 dark:text-gray-100 leading-tight focus:outline-none focus:shadow-outline" id="fileInput" name="file" type="file" required>
+
                     </div>
-                    <div class="flex items-center justify-between">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-8 rounded focus:outline-none focus:shadow-outline" type="submit">
-                            Upload
-                        </button>
-                    </div>
-                    </form>
-                    <div id="progressContainer" class="w-full bg-gray-200 mt-4 rounded hidden">
+                    <div id="progressContainer" class="w-full bg-gray-200 mt-2 rounded hidden">
                         <div id="progressBar" class="bg-blue-500 h-2 rounded"></div>
                     </div>
                     <div id="message" class="mt-2 text-gray-700 dark:text-gray-100"></div>
@@ -32,52 +51,52 @@
         </div>
     </div>
     @section('js')
-    <script>
-        const form = document.getElementById('uploadForm');
-        const fileInput = document.getElementById('fileInput');
-        const progressBar = document.getElementById('progressBar');
-        const progressContainer = document.getElementById('progressContainer');
-        const message = document.getElementById('message');
+        <script>
+            const form = document.getElementById('uploadForm');
+            const fileInput = document.getElementById('fileInput');
+            const progressBar = document.getElementById('progressBar');
+            const progressContainer = document.getElementById('progressContainer');
+            const message = document.getElementById('message');
 
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            progressContainer.classList.remove('hidden'); // Remove the 'hidden' class
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+                progressContainer.classList.remove('hidden'); // Remove the 'hidden' class
 
-            const formData = new FormData(form);
+                const formData = new FormData(form);
 
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', form.action);
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', form.action);
 
-            xhr.upload.onprogress = (e) => {
-                if (e.lengthComputable) {
-                    const percent = Math.round((e.loaded / e.total) * 100);
-                    progressBar.style.width = `${percent}%`;
-                }
-            };
+                xhr.upload.onprogress = (e) => {
+                    if (e.lengthComputable) {
+                        const percent = Math.round((e.loaded / e.total) * 100);
+                        progressBar.style.width = `${percent}%`;
+                    }
+                };
 
-            xhr.onload = () => {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    const response = JSON.parse(xhr.responseText);
-                    const id = response.id
-                    message.textContent = 'File uploaded successfully!';
-                    setTimeout(() => {
-                        window.location.href = `{{ route('process.index') }}/${id}/edit`;
-                    }, 2000); // Redirect after 2 seconds
-                } else {
+                xhr.onload = () => {
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        const response = JSON.parse(xhr.responseText);
+                        const sid = response.sid
+                        message.textContent = 'File uploaded successfully!';
+                        setTimeout(() => {
+                            window.location.href = `{{ route('upload') }}/${sid}/trim`;
+                        }, 2000); // Redirect after 2 seconds
+                    } else {
+                        message.textContent = 'An error occurred while uploading the file.';
+                    }
+                };
+
+                xhr.onerror = () => {
                     message.textContent = 'An error occurred while uploading the file.';
-                }
-            };
+                };
 
-            xhr.onerror = () => {
-                message.textContent = 'An error occurred while uploading the file.';
-            };
+                xhr.send(formData);
+            });
 
-            xhr.send(formData);
-        });
-
-        fileInput.addEventListener('change', () => {
-            progressContainer.classList.remove('hidden'); // Remove the 'hidden' class
-        });
-    </script>
+            fileInput.addEventListener('change', () => {
+                progressContainer.classList.remove('hidden'); // Remove the 'hidden' class
+            });
+        </script>
     @endsection
 </x-app-layout>
