d2cfb1ef3405c2df7c14e42567de37251f4c9af5
cp6 <cp6@users.noreply.github.com>
2024-04-11 22:43:11 +1000

    Added Tag assigned

---
+++ app/Http/Controllers/TagAssignedController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TagAssigned;
+use Illuminate\Http\Request;
+
+class TagAssignedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TagAssigned $tagAssigned)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TagAssigned $tagAssigned)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TagAssigned $tagAssigned)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TagAssigned $tagAssigned)
+    {
+        //
+    }
+}
