c9a06c7f6b6d2af1328d618cb6858e6aeca571ea
cp6 <cp6@users.noreply.github.com>
2024-04-16 23:00:00 +1000

    Added validation to TagController.php and TagAssignedController.php

--- app/Http/Controllers/TagAssignedController.php
+++ app/Http/Controllers/TagAssignedController.php
@@ -7,57 +7,45 @@ use Illuminate\Http\Request;
 
 class TagAssignedController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'tag_id' => 'integer|required',
+            'media_id' => 'integer|required'
+        ]);
+
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(TagAssigned $tagAssigned)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(TagAssigned $tagAssigned)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, TagAssigned $tagAssigned)
     {
-        //
+        $request->validate([
+            'tag_id' => 'integer|required',
+            'media_id' => 'integer|required'
+        ]);
+
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(TagAssigned $tagAssigned)
     {
         //
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -7,57 +7,43 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'string|required|max:32'
+        ]);
+
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Tag $tag)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Tag $tag)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Tag $tag)
     {
-        //
+        $request->validate([
+            'name' => 'string|required|max:32'
+        ]);
+
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Tag $tag)
     {
         //
