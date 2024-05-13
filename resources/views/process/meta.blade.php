65397165e4173e7c98804e1c627b471e8815f6d6
cp6 <cp6@users.noreply.github.com>
2024-05-13 22:37:28 +1000

    Updated meta.blade.php

--- resources/views/process/meta.blade.php
+++ resources/views/process/meta.blade.php
@@ -12,7 +12,7 @@
                     <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Meta</h5>
                 </div>
                 <div class="flow-root">
-                    <form class="space-y-6" action="#">
+                    <form class="space-y-6" action="{{route('meta.store', $media->upload->sid)}}" method="POST">
                         @csrf
                         <div>
                             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
@@ -28,9 +28,9 @@
                                       class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{$media->meta->description}}</textarea>
                         </div>
                         <div>
-                            <label for="second_ratio"
+                            <label for="visibility"
                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Visibility</label>
-                            <select id="second_ratio" name="second_ratio"
+                            <select id="visibility" name="visibility"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="0">Me only</option>
                                 <option value="1">Hidden</option>
