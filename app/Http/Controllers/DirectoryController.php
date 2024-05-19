e53dc6caaf6a38a303ef1f8de33409bac4836d3a
cp6 <cp6@users.noreply.github.com>
2024-05-19 21:21:44 +1000

    Removed unused methods in DirectoryController.php

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -7,38 +7,5 @@ use Illuminate\Http\Request;
 
 class DirectoryController extends Controller
 {
-    public function index()
-    {
-        //
-    }
 
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show(Directory $directory)
-    {
-        //
-    }
-
-    public function edit(Directory $directory)
-    {
-        //
-    }
-
-    public function update(Request $request, Directory $directory)
-    {
-        //
-    }
-
-    public function destroy(Directory $directory)
-    {
-        //
-    }
 }
